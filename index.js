07c8a6a5b00b78d4eff134931512d6a8f6e5d585
Richard H Lee <ricardohenrylee@gmail.com>
2026-05-30 11:39:53 +0100

    scoring

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ new class {
   static get SIZE() {return 12;}
   static get FOCUS() {return 2;}
   static get COUNT() {return 1;}
-  static get SIZE_WINDOW() {return 1000;}
+  static get SIZE_SMOOTHING() {return 200;}
+  static get SIZE_LISTENING() {return 1000;}
   static get SIZE_BUFFER() {return 256;}
   static get CLEARANCE() {return 5;}
   static get THRESHOLD() {return 1;}
   static get CUTOFF() {return 3;}
-  static get TOLERANCE() {return 1;}
+  static get HOLD() {return 3000;}
+  static get SCORE_MAXIMUM() {return 2;}
 
   constructor(_canvas) {this.canvas = _canvas;}
 
@@ -47,7 +49,8 @@ new class {
       () => ({used: false, time: null, data: new Float32Array(count)})
     );
     this.end = 0;
-    this.start = 1;
+    this.smoothing = 1;
+    this.listening = 1;
     const clearance = constructor.CLEARANCE;
     this.boundLower = clearance * 2 - 1;
     this.boundUpper = width - clearance;
@@ -57,26 +60,26 @@ new class {
     this.context = _canvas.getContext('2d');
     _canvas.width = width;
     _canvas.height = analyser.maxDecibels - analyser.minDecibels;
-    const binsRaw = localStorage.getItem('bins');
-    if (binsRaw) {
-      const bins = JSON.parse(binsRaw);
-      console.log(bins);
-      const sigma = this.constructor.CLEARANCE / 3;
-      const peaks = [];
-      for (let peak = bins.offset; peak < width; peak += bins.interval)
-        peaks.push(Math.round(peak));
-      const distribution = new Array(width * 2);
-      const peakCount = peaks.length;
-      for (let bin = -width; bin < width; bin++)
-        distribution[bin + width]
-          = Math.exp(((bin / sigma) ** 2) / -2);
-      const weights = new Array(width).fill(0);
-      for (let bin = 0; bin < width; bin++)
-        for (const peak of peaks)
-          weights[bin] += distribution[bin + width - peak];
-      this.weights = weights;
-      console.log(weights);
+
+    const target = JSON.parse(localStorage.getItem('target'));
+    this.target = target;
+    if (target) {
+      this.background = 'black';
+      this.sizeSmoothing = constructor.SIZE_SMOOTHING;
+    } else {
+      this.background = 'gray';
+      this.sizeSmoothing = constructor.SIZE_LISTENING;
     }
+
+    this.save = false;
+    document.onpointerdown = event => {this.downtime = event.timeStamp;};
+    document.onpointerup = event => {
+      if (event.timeStamp - this.downtime > this.constructor.HOLD)
+        localStorage.clear();
+      else this.save = true;
+    };
+
+    this.criterion = new criteria.amplitude(clearance);
 
     const renderBound = this.render.bind(this);
     this.renderBound = renderBound;
@@ -84,11 +87,21 @@ new class {
   }
 
   render(time) {
+    const context = this.context;
+    const _canvas = this.canvas;
+    const width = _canvas.width;
+    const height = _canvas.height;
+    const constructor = this.constructor;
+    const analyser = this.analyser;
+    const ceiling = analyser.maxDecibels;
+    const floor = analyser.minDecibels;
+    const peakBins = new Array(width);
     const peaks = [];
-    {
-      const constructor = this.constructor;
-      const analyser = this.analyser;
+    const averages = new Float32Array(width);
+    const clearance = constructor.CLEARANCE;
+    const clearanceWidth = clearance * 2 + 1;
 
+    {
       const _window = this.window;
       const lengthWindow = _window.length;
       this.end = (this.end + 1) % lengthWindow;
@@ -98,22 +111,15 @@ new class {
       frame.time = time;
       const data = frame.data;
       analyser.getFloatFrequencyData(data);
-      while (time - _window[this.start].time > constructor.SIZE_WINDOW) {
-        _window[this.start].used = false;
-        this.start = (this.start + 1) % lengthWindow;
+      while (time - _window[this.smoothing].time > constructor.SIZE_SMOOTHING)
+        this.smoothing = (this.smoothing + 1) % lengthWindow;
+      while (time - _window[this.listening].time > constructor.SIZE_LISTENING)
+      {
+        _window[this.listening].used = false;
+        this.listening = (this.listening + 1) % lengthWindow;
       }
 
-      const context = this.context;
-      const _canvas = this.canvas;
-      const width = _canvas.width;
-      const height = _canvas.height;
-      const ceiling = analyser.maxDecibels;
-      const floor = analyser.minDecibels;
-      context.clearRect(0, 0, width, height);
-      context.fillStyle = 'black';
-      context.fillRect(0, 0, width, height);
-
-      const start = this.start;
+      const start = this.smoothing;
       const thisEnd = this.end;
       const end = 1 + (thisEnd > start? thisEnd : thisEnd + lengthWindow);
       const total = new Float32Array(width);
@@ -125,56 +131,44 @@ new class {
 
       const count = end - start;
       let average;
-      const averages = new Float32Array(width);
       const boundLower = this.boundLower;
       const boundUpper = this.boundUpper;
-      const clearance = constructor.CLEARANCE;
       let middle;
       let indexMiddle;
       const threshold = this.threshold;
-      const clearanceWidth = clearance * 2 + 1;
-      const peaksBoolean = new Array(width);
-      let peak;
+      let slice;
+      const criterion = this.criterion;
+      const evaluate = criterion.evaluate.bind(criterion);
 
       for (let indexBin = 0; indexBin < width; indexBin++) {
         average = averages[indexBin] = total[indexBin] / count;
         if (indexBin > boundLower && indexBin < boundUpper) {
           indexMiddle = indexBin - clearance;
           middle = averages[indexMiddle];
-          peaksBoolean[indexMiddle]
-            = middle > threshold &&
-            averages[indexMiddle - 1] < middle &&
-            middle > averages[indexMiddle + 1]
-              && Math.max(
-                ...averages.slice(indexBin + 1 - clearanceWidth, indexBin + 1)
-              )
-              === middle &&
-            peaks.push(indexMiddle);
+          slice = averages.slice(indexBin + 1 - clearanceWidth, indexBin + 1);
+          if (
+            middle > threshold
+            && averages[indexMiddle - 1] < middle
+            && middle > averages[indexMiddle + 1]
+            && Math.max(...slice) === middle
+          ) peaks.push(
+            peakBins[indexMiddle] = {bin: indexMiddle, score: evaluate(slice)}
+          );
+          else peakBins[indexMiddle] = null;
         }
-      }
-
-      const weights = this.weights;
-      let weight;
-      for (let indexBin = 0; indexBin < width; indexBin++) {
-        context.fillStyle = 'purple';
-        weight = weights[indexBin] * height;
-        context.fillRect(indexBin, height - weight, 1, weight);
-        if (peaksBoolean[indexBin]) {
-          context.fillStyle = 'orange';
-          context.fillRect(indexBin, 0, 1, height);
-          context.fillStyle = 'red';
-        }
-        average = averages[indexBin];
-        context.fillStyle = 'red';
-        context.fillRect(indexBin, ceiling - average, 1, average - floor);
       }
     }
+
+    context.clearRect(0, 0, width, height);
+    context.fillStyle = this.background;
+    context.fillRect(0, 0, width, height);
 
     const lengthPeaks = peaks.length;
     if (lengthPeaks > 1) {
       const differences = new Array(lengthPeaks * (lengthPeaks - 1) / 2);
+      const peaksBin = peaks.map(peak => peak.bin);
       const differencePeaks = new Array
-        (Math.max(...peaks) - Math.min(...peaks) + 1).fill(Infinity);
+        (Math.max(...peaksBin) - Math.min(...peaksBin) + 1).fill(Infinity);
       {
         let differenceIndex = 0;
         let second;
@@ -183,8 +177,8 @@ new class {
         let peakSecond;
         for (let first = 1; first < lengthPeaks; first++)
           for (second = 0; second < first; second++) {
-            peakFirst = peaks[first];
-            peakSecond = peaks[second];
+            peakFirst = peaks[first].bin;
+            peakSecond = peaks[second].bin;
             difference = Math.abs(peakFirst - peakSecond);
             differences[differenceIndex++] = difference;
             differencePeaks[difference]
@@ -237,30 +231,73 @@ new class {
         = Math.min(...cluster.map(difference => differencePeaks[difference]));
       let interval = total / count;
       peaks.splice(peaks.indexOf(offset), 1);
-      const tolerance = this.constructor.TOLERANCE;
-      let distance;
-      let deviation;
-      for (let peak of peaks) {
-        distance = Math.abs(peak - offset);
-        deviation = distance % interval;
-        if (deviation <= tolerance || interval - deviation <= tolerance)
-          interval = distance / Math.round(distance / interval);
-      }
 
-      console.log(offset % interval, interval, peaks.length);
+      const target = this.target;
+      let extra;
+      if (target) {
+        const interval = target.interval;
+        const score
+          = peaks.length / target.peaks
+          * (1 - Math.abs(interval - interval) / interval)
+          / 2;
+
+        context.fillStyle = 'gray';
+        context.fillRect(0, height * (1 - score), width, height * score);
+
+        extra = ["", `score: ${score}`];
+      } else extra = [];
+      output.innerHTML = [
+        ...[
+          `offset: ${offset % interval}`,
+          `interval: ${interval}`,
+          `peaks: ${peaks.length}`,
+        ],
+        ...extra
+      ].join("\n");
+
+      if (this.save) {
+        localStorage.setItem(
+          'target',
+          JSON.stringify({
+            offset: interval % offset, interval: interval, peaks: peaks.length
+          })
+        );
+        this.save = false;
+      };
+    }
+
+    {
+      let average;
+      let peak;
+      for (let indexBin = 0; indexBin < width; indexBin++) {
+        peak = peakBins[indexBin];
+        if (peak) {
+          context.fillStyle = 'purple';
+          context.fillRect(indexBin, 0, 1, height);
+        }
+        average = averages[indexBin];
+        context.fillStyle = 'red';
+        context.fillRect(indexBin, ceiling - average, 1, average - floor);
+        if (peak) {
+          context.fillStyle = 'green';
+          console.log(peak.score);
+          context.fillRect(indexBin, height - peak.score, 1, peak.score);
+        }
+      }
     }
 
     requestAnimationFrame(this.renderBound);
   }
-
-  log(message) {
-    fetch(
-      "",
-      {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(message)
-      }
-    );
-  }
 }(canvas).load();
+
+const criteria = {
+  'amplitude': class {
+    constructor(clearance) {
+      this.clearance = clearance;
+    }
+
+    evaluate(peaks) {
+      return peaks[this.clearance] - Math.min(...peaks);
+    }
+  }
+};
